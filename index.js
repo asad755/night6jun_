b592cf9988e5eb67dcbe8e058dcbc4eb59761d14
Asadiftekhar <143116178+asad755@users.noreply.github.com>
2024-06-07 04:45:36 +0500

    wellwell

---
+++ index.js
@@ -0,0 +1,3 @@
+import { b, c } from "./secend.js";
+//console.log(a );
+console.log(b + c);
---
+++ index.ts
@@ -0,0 +1,6 @@
+//import a from "./first.js";
+import a from "./first.js";
+import {b, c} from "./secend.js";
+
+//console.log(a );
+console.log(b + c);
